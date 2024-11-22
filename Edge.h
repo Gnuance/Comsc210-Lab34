0a8d90c8269941783c8be8a94e3529710edea194
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-11-22 13:48:42 -0800

    10 minute timestamp

--- Edge.h
+++ Edge.h
@@ -1,6 +1,7 @@
 #ifndef EDGE_H
 #define EDGE_H
 
+#include <set>
 using namespace std;
 
 class Edge
@@ -9,9 +10,19 @@ public:
     int src, dest, weight;
 
     // Static function to get largest vertex within a container of edge
-    template <typename Container<Edge>>
-    static int getMaxVertex(const Container<Edge>& c){
+    template <typename Container>
+    static int getMaxVertex(const Container& container){
+        
+        // List of nodes connected by edges
+        set<int> nodes;
 
+        // Add all unique nodes to set
+        for (const auto edge : container)
+        {
+            nodes.insert(edge.src);
+            nodes.insert(edge.dest);
+        }
+        
     }
 };
 
