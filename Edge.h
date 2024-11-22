f88d2e566dce478f52f45a321e5de70f3227deea
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-11-22 13:28:39 -0800

    10 minute timestamp

---
+++ Edge.h
@@ -0,0 +1,17 @@
+#ifndef EDGE_H
+#define EDGE_H
+
+using namespace std;
+
+class Edge
+{
+public:
+    int src, dest, weight;
+
+    template <typename Container>
+    static int getMaxVertex(const Container& c){
+
+    }
+};
+
+#endif
--- NetworkGraph.h
+++ NetworkGraph.h
@@ -2,9 +2,50 @@
 #define NETWORK_GRAPH_H
 #include <iostream>
 #include <iomanip>
+#include <vector>
+#include "Edge.h"
 using namespace std;
 
+typedef pair<int, int> Pair; // Creates alias 'Pair' for the pair<int,int> data type
 
+class Graph
+{
+public:
+    // a vector of vectors of Pairs to represent an adjacency list
+    vector<vector<Pair>> adjList;
 
+    // Graph Constructor
+    Graph(vector<Edge> const &edges)
+    {
+        // resize the vector to hold SIZE elements of type vector<Edge>
+        adjList.resize(SIZE);
+
+        // add edges to the directed graph
+        for (auto &edge : edges)
+        {
+            int src = edge.src;
+            int dest = edge.dest;
+            int weight = edge.weight;
+
+            // insert at the end
+            adjList[src].push_back(make_pair(dest, weight));
+            // for an undirected graph, add an edge from dest to src also
+            adjList[dest].push_back(make_pair(src, weight));
+        }
+    }
+
+    // Print the graph's adjacency list
+    void printGraph()
+    {
+        cout << "Graph's adjacency list:" << endl;
+        for (int i = 0; i < adjList.size(); i++)
+        {
+            cout << i << " --> ";
+            for (Pair v : adjList[i])
+                cout << "(" << v.first << ", " << v.second << ") ";
+            cout << endl;
+        }
+    }
+};
 
 #endif
